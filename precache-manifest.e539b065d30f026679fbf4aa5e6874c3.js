fa1def889d964ec7b8563c1d0a3bd1cad3b4054f
Marijana Miljkovic <marijana308@gmail.com>
2020-10-04 20:31:08 +0200

    Updates

---
+++ precache-manifest.e539b065d30f026679fbf4aa5e6874c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a89095f3a12f5c8c7eff787d5103a0",
+    "url": "/CatchEmAll/index.html"
+  },
+  {
+    "revision": "92b9dc5956ff2f041e6f",
+    "url": "/CatchEmAll/static/css/main.61006acf.chunk.css"
+  },
+  {
+    "revision": "de9248a097b42a9eba42",
+    "url": "/CatchEmAll/static/js/2.fec5c6c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CatchEmAll/static/js/2.fec5c6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b9dc5956ff2f041e6f",
+    "url": "/CatchEmAll/static/js/main.74dfca91.chunk.js"
+  },
+  {
+    "revision": "2f0c4077fef82cdd3af6",
+    "url": "/CatchEmAll/static/js/runtime-main.50906ac9.js"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/CatchEmAll/static/media/PokemonHollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/CatchEmAll/static/media/PokemonSolid.36d26dbc.ttf"
+  },
+  {
+    "revision": "8f80a92d7ba78cb0e5a3e30927022f31",
+    "url": "/CatchEmAll/static/media/pokemon-logo.8f80a92d.png"
+  }
+]);
